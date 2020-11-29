d4cb52426087b2cc43b19b1f0c7fb75d1a8ebea9
Olatunji Joshua Kayode <19627177+OkayJosh@users.noreply.github.com>
2020-11-29 21:37:50 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 python manage.py migrate
-gunicorn accountingfast.wsgi:application --bind 0.0.0.0:8000 --log-level info --timeout 180  --workers 3
+gunicorn accounting.wsgi:application --bind 0.0.0.0:8000 --log-level info --timeout 180  --workers 3
