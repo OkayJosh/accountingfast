86ba62803d2b1b6944c3ec8866efa51b3b14c269
Olatunji Joshua Kayode <19627177+OkayJosh@users.noreply.github.com>
2020-11-29 21:35:57 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 python manage.py migrate
-gunicorn kubernetes_django.wsgi:application --bind 0.0.0.0:8000 --log-level info --timeout 180  --workers 3
+gunicorn accountingfast.wsgi:application --bind 0.0.0.0:8000 --log-level info --timeout 180  --workers 3
